ed9e7ccc998759ec4cb23d79aa1a4e11db5025ea
root <root@sameervivobook>
2024-03-12 04:42:47 +0530

    added photo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,10 +9,10 @@
     ];
 </script>
 
-<header class={'sticky z-[10]  top-0  duration-200 flex items-center justify-between border border-solid'+(y>0?" py-4 bg-slate-950  border-b-violet-950":" py-6 bg-transparent border-transparent")} >
+<header class={'sticky z-[10] shadow-lg  shadow-purple-500/50  top-0  duration-200 flex items-center justify-between border border-solid'+(y>0?" py-4 bg-slate-950  border-b-violet-950":" py-6 bg-transparent border-transparent")} >
     <a href="/" class="flex items-center gap-2">
-    <h1 class="font-medium ml-2">
-        <span class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-extrabold poppins">Sameer Yadav</span>
+    <h1 class="font-medium ml-2  ">
+        <span class="bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-extrabold poppins">Sameer Yadav</span>
     </h1>
     </a>
     <div class="sm:flex items-center gap-4 hidden">
@@ -23,6 +23,7 @@
             {/each}
     </div>
     <!-- Get in touch -->
-    <a href="/contact" class="px-4 py-2 bg-white text-black rounded-full hover:bg-violet-900 duration-200">
-        <h4 class="relative z-[9] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold poppins">Get in touch</h4></a>
+    <a href="/contact" class="px-4 py-2 shadow-lg shadow-indigo-500/50  bg-white text-black rounded-full hover:bg-violet-900 duration-200">
+        <h4 class="relative z-[9] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold poppins">Get in touch</h4>
+    </a>
 </header>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -12,37 +12,29 @@
                 <h1 class="text-4xl font-bold ">Hi! I'm <span class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent poppins">Sameer Yadav</span>
                 <br/>Full stack <span class=" text-violet-500 poppins">Developer !</span> 
                 </h1>
-                <p class="text-base sm:text-lg md:text-xl">
+                <p class="text-base sm:text-lg md:text-xl py-3">
                     My favorite technologies include:
                     <li class=''><span class="text-violet-500 poppins">Web Development:</span><br/>Python(Flask),JS(Svelte),Tailwind CSS,NodeJs</li>                    
                     <li class=''><span class="text-violet-500 poppins">Mobile Development:</span> Dart (Flutter),Firebase</li>
                     <li class=''><span class="text-violet-500 poppins">Machine Learning:</span> Python (Tensorflow, PyTorch)</li> 
                     <li class=''><span class="text-violet-500 poppins">Databases:</span> SQL(Oracle,Mysql,sqlite3), NoSQL</li>
-                    I have extensive experience working on a variety of projects and deploying them.
                 </p>
-            
-            
+                <!--
+                    create a link button to get in touch
+                -->
+                <a href="/contact" target="_blank" class="mx-auto lg:mr-auto sm:text-lg md:text-xl text-base poppins relative overflow-hidden  px-4 py-2  bg-white rounded-full hover:bg-violet-900 duration-200 cursor-pointer">
+                    <div class="absolute top-0 left-0 w-full h-full opacity-20  group-hover:translate-x-full duration-200"/>
+                    <h4 class="relative z-[9] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold poppins">Get in touch <span class="ml-2">&#8594;</span>
+                    </h4>
+                </a>
             </div>
-        </div>      
-        
-    <h1 class="text-4xl font-bold">Projects</h1>
+            
+        </div>    
+        <div class="justify-center relative grid place-items-center">
+            <img src="/images/profile.png" alt="Sameer Yadav" class="object-cover z-[2] max-h-[72vh]" />
+          </div>
+         
     </section>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-    <h1 class="text-4xl font-bold">Projects</h1>
-    <p class="text-lg">I am a full stack developer and I have worked on a variety of projects. Here are some of the projects that I have worked on.</p>
-
+    
 </main>
     <slot />
