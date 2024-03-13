8e6bc4165ff1e700953c5dcdcbe29ae00a2e5a90
root <root@sameervivobook>
2024-03-13 16:18:55 +0530

    button fix

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -34,7 +34,7 @@
                 <!--
                     create a link button to get in touch
                 -->
-                <a href="#contact" target="_blank" class="mx-auto   lg:mr-auto sm:text-lg md:text-xl lg:ml-0 text-base poppins relative overflow-hidden  px-4 py-2  bg-white rounded-full hover:bg-violet-900 duration-200 cursor-pointer">
+                <a href="#contact"  class="mx-auto   lg:mr-auto sm:text-lg md:text-xl lg:ml-0 text-base poppins relative overflow-hidden  px-4 py-2  bg-white rounded-full hover:bg-violet-900 duration-200 cursor-pointer">
                     <div class="absolute top-0 left-0 w-full h-full opacity-20  group-hover:translate-x-full duration-200"/>
                     <h4 class="relative z-[9] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold poppins">Get in touch <span class="ml-2">&#8594;</span>
                     </h4>
